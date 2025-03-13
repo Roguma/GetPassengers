095e8e0b4d98e51db71fb6e808198c22b50569bf
Roguma <roguma@msudenver.edu>
2025-03-12 19:08:36 -0600

    Initial commit

---
+++ app/src/main/java/edu/msudenver/cs3013/getpassengers/GetPassengers.kt
@@ -0,0 +1,16 @@
+package edu.msudenver.cs3013.getpassengers
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class GetPassengers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_get_passengers)
+
+    }
+}
